a3fbb279aa2bd8e4f865ed7da4fbd6b347c54115
tsyganovni <nikita--93@mail.ru>
2026-05-05 17:34:10 +0300

    fix(client)

--- app/server-connector/index.ts
+++ app/server-connector/index.ts
@@ -811,7 +811,7 @@ import { io } from 'socket.io-client'
 import Queue from 'yocto-queue'
 
 //const HOST = 'http://localhost:3000' // ''
-const HOST = 'http://localhost:3000' // ''
+const HOST = '' // 'http://localhost:3000' // ''
 const PATH_PREFIX = '/api'
 const SECS = 1000
 const MINS = 60 * SECS
